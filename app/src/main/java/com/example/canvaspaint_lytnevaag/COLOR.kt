db5da508ae9900cdb26a827e7a1ac8441bb8771f
Anastasiya Lytneva <kvartira-62@yandex.ru>
2022-08-24 00:57:36 +0300

    - в отдельные enumClasses выведены цвета и размер в

---
+++ app/src/main/java/com/example/canvaspaint_lytnevaag/COLOR.kt
@@ -0,0 +1,22 @@
+package com.example.canvaspaint_lytnevaag
+
+import androidx.annotation.ColorRes
+
+
+enum class COLOR(
+    @ColorRes
+    val value: Int
+) {
+
+    BLACK(R.color.black),
+    RED(R.color.purple_500),
+    BLUE(R.color.purple_700),
+    PINK(R.color.teal_700),
+    GREEN (R.color.teal_200);
+
+
+    companion object {
+        private val map = values().associateBy(COLOR::value)
+        fun from(color: Int) = map[color] ?: GREEN
+    }
+}
